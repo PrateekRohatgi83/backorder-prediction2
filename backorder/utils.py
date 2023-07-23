182d5d49058539c98734fe7fc7aada374ba6711e
PrateekRohatgi83 <rohatgiprateek2014@gmail.com>
2023-07-23 15:33:30 +0530

    updated utility

--- backorder/utils.py
+++ backorder/utils.py
@@ -27,3 +27,19 @@ def export_collection_as_dataframe(database_name:str, collection_name:str)->pd.D
         return df
     except Exception as e:
         raise BackorderException(e, sys)
+
+def write_yaml_file(file_path, data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir, exist_ok=True)
+        with open(file_path, 'w') as file_writer:
+            yaml.dump(data, file_writer)
+    except Exception as e:
+            raise BackorderException(e, sys)
+
+def read_yaml_file(file_path):
+    try:
+        with open(file_path, 'rb') as file_reader:
+            yaml.safe_load(file_reader)
+    except Exception as e:
+            raise BackorderException(e, sys)
